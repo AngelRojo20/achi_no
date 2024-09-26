9523d64645e0fd969cef99381d7a1cf9eadbc3aa
Miguel Bohada <miguelbohada_8@hotmail.com>
2024-09-26 17:37:26 -0500

    segundo commit

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
-let next = document.querySelector('.next')
-let prev = document.querySelector('.prev')
+let next = document.querySelector('.next');
+let prev = document.querySelector('.prev');
 
 next.addEventListener('click', function(){
-    let items = document.querySelectorAll('.item')
-    document.querySelector('.slide').appendChild(items[0])
-})
+    let items = document.querySelectorAll('.item');
+    document.querySelector('.slide').appendChild(items[0]);
+});
 
 prev.addEventListener('click', function(){
-    let items = document.querySelectorAll('.item')
-    document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
-})
+    let items = document.querySelectorAll('.item');
+    document.querySelector('.slide').prepend(items[items.length - 1]); // here the length of items = 6
+});
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'ArrowRight') {  // Flecha derecha
+        let items = document.querySelectorAll('.item');
+        document.querySelector('.slide').appendChild(items[0]);
+    } else if (event.key === 'ArrowLeft') {  // Flecha izquierda
+        let items = document.querySelectorAll('.item');
+        document.querySelector('.slide').prepend(items[items.length - 1]);
+    }
+});
